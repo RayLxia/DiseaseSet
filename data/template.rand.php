1922a9b9edc9b74e57143a388a6c8fc70e6c8b60
RayXia <1556640339@qq.com>
2017-01-08 09:20:17 +0800

    Initial commit

---
+++ data/template.rand.php
@@ -0,0 +1,11 @@
+<?php
+
+//���ֵΪ 0 ��ʾ�رմ����ã� Ϊ 1 ��ʾ����
+$cfg_tamplate_rand = 0;
+
+//ģ�����飬�����Ҫ���ӣ��������ʽ���ӻ��޸ļ���(����ȷ����Щģ���Ǵ��ڵ�)��������������Ϊ2�������ϡ�
+$cfg_tamplate_arr[] = 'article_article222.htm';
+$cfg_tamplate_arr[] = 'article_article1.htm';
+$cfg_tamplate_arr[] = 'article_article2.htm';
+
+?>
